acf064ee4867adc86724061b526c1b573f081228
Furkan Dogmus <furkan.dogmus@btsgrp.com>
2026-04-11 18:16:39 +0300

    fix errors

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,16 +1,5 @@
 import {getRequestConfig} from 'next-intl/server';
 import {routing} from './routing';
-import {deepMergeMessages} from './merge-messages';
-
-const MERGE_LOCALES = new Set([
-  'de',
-  'fr',
-  'es',
-  'it',
-  'zh',
-  'ja',
-  'ar'
-]);
 
 export default getRequestConfig(async ({requestLocale}) => {
   let locale = await requestLocale;
@@ -20,18 +9,6 @@ export default getRequestConfig(async ({requestLocale}) => {
     !routing.locales.includes(locale as (typeof routing.locales)[number])
   ) {
     locale = routing.defaultLocale;
-  }
-
-  if (MERGE_LOCALES.has(locale)) {
-    const [baseMod, overlayMod] = await Promise.all([
-      import('../locales/en.json'),
-      import(`../locales/${locale}.json`)
-    ]);
-    const messages = deepMergeMessages(
-      baseMod.default as Record<string, unknown>,
-      overlayMod.default as Record<string, unknown>
-    );
-    return {locale, messages};
   }
 
   return {
