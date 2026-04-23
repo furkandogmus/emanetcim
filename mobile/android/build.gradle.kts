210e4f05e2eff96f7fae3d1349e047ca05841813
Furkan Dogmus <furkan.dogmus@btsgrp.com>
2026-04-23 18:59:16 +0300

    fix: force kotlin language version 1.8 for all subprojects to fix sentry_flutter build

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -30,6 +30,15 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            languageVersion = "1.8"
+            apiVersion = "1.8"
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
